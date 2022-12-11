b241a41be7527f4344c474c1f9894d185209247d
Norbert Czarnota <czarny-s@o2.pl>
2022-12-11 12:45:22 +0100

    Upload image API functinality added.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -92,4 +92,14 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Seriazlier for recipe detail view."""
 
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ['description', 'image']
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading iamges to recipes."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
+        read_only_fields = ['id']
+        extra_kwargs = {'image': {'required': 'True'}}
